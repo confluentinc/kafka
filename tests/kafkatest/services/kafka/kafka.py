b5786bd153e4050501a1edcf4b7b57c11d2de854
Ismael Juma <ismael@juma.me.uk>
2020-02-04 11:55:37 -0800

    Fix pids command

--- tests/kafkatest/services/kafka/kafka.py
+++ tests/kafkatest/services/kafka/kafka.py
@@ -368,7 +368,7 @@ class KafkaService(KafkaPathResolverMixin, JmxMixin, Service):
     def pids(self, node):
         """Return process ids associated with running processes on the given node."""
         try:
-            cmd = "jcmd | grep -e %s -e %s | awk '{print $1}'" % self.java_class_name()
+            cmd = "jcmd | grep -e %s | awk '{print $1}'" % self.java_class_name()
             pid_arr = [pid for pid in node.account.ssh_capture(cmd, allow_fail=True, callback=int)]
             return pid_arr
         except (RemoteCommandError, ValueError) as e:
