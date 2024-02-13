78a6d5ddf16b126b2551980cf622a81841a855c7
Sushant Mahajan <smahajan@confluent.io>
2024-02-13 16:40:52 +0530

    Completed implementation per the schema.

--- group-coordinator/src/main/java/org/apache/kafka/coordinator/group/share/ShareGroupMember.java
+++ group-coordinator/src/main/java/org/apache/kafka/coordinator/group/share/ShareGroupMember.java
@@ -17,8 +17,12 @@
 package org.apache.kafka.coordinator.group.share;
 
 import org.apache.kafka.common.Uuid;
+import org.apache.kafka.common.message.ShareGroupDescribeResponseData;
 import org.apache.kafka.coordinator.group.GroupMember;
+import org.apache.kafka.image.TopicImage;
+import org.apache.kafka.image.TopicsImage;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -235,5 +239,54 @@ public class ShareGroupMember extends GroupMember {
             ", state=" + state +
             ", assignedPartitions=" + assignedPartitions +
             ')';
+  }
+
+  private static String lookupTopicNameById(
+      Uuid topicId,
+      TopicsImage topicsImage
+  ) {
+    TopicImage topicImage = topicsImage.getTopic(topicId);
+    if (topicImage != null) {
+      return topicImage.name();
+    } else {
+      return null;
+    }
+  }
+
+  private static List<ShareGroupDescribeResponseData.TopicPartitions> topicPartitionsFromMap(
+      Map<Uuid, Set<Integer>> partitions,
+      TopicsImage topicsImage
+  ) {
+    List<ShareGroupDescribeResponseData.TopicPartitions> topicPartitions = new ArrayList<>();
+    partitions.forEach((topicId, partitionSet) -> {
+      String topicName = lookupTopicNameById(topicId, topicsImage);
+      if (topicName != null) {
+        topicPartitions.add(new ShareGroupDescribeResponseData.TopicPartitions()
+            .setTopicId(topicId)
+            .setTopicName(topicName)
+            .setPartitions(new ArrayList<>(partitionSet)));
+      }
+    });
+    return topicPartitions;
+  }
+
+  /**
+   * @param topicsImage: Topics image objec to search for a specific topic id
+   *
+   * @return The ShareGroupMember mapped as ShareGroupDescribeResponseData.Member.
+   */
+  public ShareGroupDescribeResponseData.Member asShareGroupDescribeMember(
+      TopicsImage topicsImage
+  ) {
+    return new ShareGroupDescribeResponseData.Member()
+        .setMemberEpoch(memberEpoch)
+        .setMemberId(memberId)
+        .setAssignment(new ShareGroupDescribeResponseData.Assignment()
+            .setTopicPartitions(topicPartitionsFromMap(assignedPartitions, topicsImage)))
+        .setClientHost(clientHost)
+        .setClientId(clientId)
+        .setInstanceId(instanceId)
+        .setRackId(rackId)
+        .setSubscribedTopicNames(subscribedTopicNames);
   }
 }
