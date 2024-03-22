12b808a55b78f3dd9df813826f4c95bf4398e00d
adixitconfluent <adixit@confluent.io>
2024-03-22 11:03:50 +0530

    Reverting the fixes to build since they were taken care of in PR#1121

--- core/src/test/scala/unit/kafka/utils/TestUtils.scala
+++ core/src/test/scala/unit/kafka/utils/TestUtils.scala
@@ -1938,7 +1938,7 @@ object TestUtils extends Logging {
       records ++= polledRecords.asScala
       records.size >= numRecords
     }
-    pollRecordsUntilTrue(consumer, pollAction(_),
+    pollRecordsUntilTrue(consumer, pollAction,
       waitTimeMs = waitTimeMs,
       msg = s"Consumed ${records.size} records before timeout instead of the expected $numRecords records")
     records
