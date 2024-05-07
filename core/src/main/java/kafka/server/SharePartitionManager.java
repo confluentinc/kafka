bb6210ad152186ad9aad5ff61b6edba6be119ab4
Chirag Wadhwa <cwadhwa@confluent.io>
2024-05-07 16:22:21 +0530

    AKCORE-129: Minor changes and added unit tests

--- core/src/main/java/kafka/server/SharePartitionManager.java
+++ core/src/main/java/kafka/server/SharePartitionManager.java
@@ -162,13 +162,17 @@ public class SharePartitionManager implements AutoCloseable {
                     //    This is because in this case we don't need to check for the partition limit for in flight messages
                     // 2. If condition 1 is not true, then that means we will be fetching new records which haven't been cached before.
                     //    In this case it is necessary to check if the partition limit for in flight messages has been reached.
-                    if (sharePartition.nextFetchOffset() != sharePartition.endOffset() || sharePartition.canAcquireMore()) {
-                    topicPartitionData.put(topicIdPartition, new FetchRequest.PartitionData(
-                            topicIdPartition.topicId(),
-                            sharePartition.nextFetchOffset(),
-                            0,
-                            partitionMaxBytes,
-                            Optional.empty()));
+                    if (sharePartition.nextFetchOffset() != (sharePartition.endOffset() + 1) || sharePartition.canAcquireMore()) {
+                    topicPartitionData.put(
+                        topicIdPartition,
+                        new FetchRequest.PartitionData(
+                                topicIdPartition.topicId(),
+                                sharePartition.nextFetchOffset(),
+                                0,
+                                partitionMaxBytes,
+                                Optional.empty()
+                        )
+                    );
                     } else {
                         sharePartition.releaseFetchLock();
                         log.info("Record lock partition limit exceeded for SharePartition with key {}, " +
