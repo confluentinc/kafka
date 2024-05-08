a346c0c9130c9ff1404239c2acdd3330c955ec2d
adixitconfluent <adixit@confluent.io>
2024-05-08 14:20:00 +0530

    Fixed KafkaApisTest failures

--- core/src/test/scala/unit/kafka/server/KafkaApisTest.scala
+++ core/src/test/scala/unit/kafka/server/KafkaApisTest.scala
@@ -4405,7 +4405,7 @@ class KafkaApisTest extends Logging {
     val topicId = Uuid.randomUuid()
     val partitionIndex = 0
     addTopicToMetadataCache(topicName, 1, topicId = topicId)
-    val memberId : Uuid = Uuid.ZERO_UUID
+    val memberId : Uuid = Uuid.randomUuid()
 
     val records1 = memoryRecords(10, 0)
     val records2 = memoryRecords(10, 10)
@@ -4910,7 +4910,7 @@ class KafkaApisTest extends Logging {
     val topicId = Uuid.randomUuid()
     val partitionIndex = 0
     addTopicToMetadataCache(topicName, 1, topicId = topicId)
-    val memberId: Uuid = Uuid.ZERO_UUID
+    val memberId: Uuid = Uuid.randomUuid()
 
     val records1 = memoryRecords(10, 0)
     val records2 = memoryRecords(10, 10)
