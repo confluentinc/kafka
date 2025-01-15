46d0ac6201ef7adc6366d83384d97c4bb1e9f3cf
Dmitry Werner <grimekillah@gmail.com>
2025-01-16 02:59:28 +0500

    KAFKA-18479: Remove keepPartitionMetadataFile in UnifiedLog and LogMan… (#18491)
    Reviewers: Jun Rao <junrao@gmail.com>

--- core/src/test/scala/unit/kafka/utils/TestUtils.scala
+++ core/src/test/scala/unit/kafka/utils/TestUtils.scala
@@ -1002,7 +1002,6 @@ object TestUtils extends Logging {
                    time = time,
                    brokerTopicStats = new BrokerTopicStats,
                    logDirFailureChannel = new LogDirFailureChannel(logDirs.size),
-                   keepPartitionMetadataFile = true,
                    interBrokerProtocolVersion = interBrokerProtocolVersion,
                    remoteStorageSystemEnable = remoteStorageSystemEnable,
                    initialTaskDelayMs = initialTaskDelayMs)
