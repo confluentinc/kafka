3948f1928b0d10a425b3a23b78f11f450c7be75a
jainruchir <rujain@confluent.io>
2025-06-01 08:51:18 +0530

    Update ducktape version to 12.0

--- tests/setup.py
+++ tests/setup.py
@@ -51,7 +51,7 @@ setup(name="kafkatest",
       license="apache2.0",
       packages=find_packages(),
       include_package_data=True,
-      install_requires=["ducktape==0.12.1", "requests==2.32.2"],
+      install_requires=["ducktape==0.12.0", "requests==2.31.0"],
       tests_require=["pytest", "mock"],
       cmdclass={'test': PyTest},
       zip_safe=False
