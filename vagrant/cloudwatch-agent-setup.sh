e32ee813591b22339208e373d03f0d5359d0af46
souvikrakshit <srakshit@confluent.io>
2025-06-09 18:34:38 +0530

    download jq manually

--- vagrant/cloudwatch-agent-setup.sh
+++ vagrant/cloudwatch-agent-setup.sh
@@ -30,7 +30,7 @@ apt-get -y update && apt-get -y install collectd
 #apt-get -y update && apt-get -y install jq
 echo "current_directory_start"
 pwd
-#cd /tmp
+cd /tmp
 
 # Get the latest stable jq release URL from GitHub releases page (check for the latest version)
 # As of now, jq 1.7.1 is the latest stable.
@@ -43,8 +43,8 @@ wget "$JQ_URL" -O jq
 # Make it executable
 chmod +x jq
 
-# Move it to a directory in your PATH (e.g., /usr/local/bin)
-sudo mv jq /usr/local/bin/
+# Copy it to a directory in your PATH (e.g., /usr/local/bin)
+sudo cp jq /usr/local/bin/
 
 # Verify the version
 jq --version
@@ -52,7 +52,6 @@ jq --version
 ###
 echo "current_directory_end"
 pwd
-#cd /home/ubuntu
 
 CONFIG_FILE="/tmp/cloudwatch-agent-configuration.json"
 TEMP_FILE="${CONFIG_FILE}.tmp"
