d3e0219070379a201c5755a043f489dd4ad94acc
souvikrakshit <srakshit@confluent.io>
2025-06-09 20:23:27 +0530

    download jq manually

--- vagrant/cloudwatch-agent-setup.sh
+++ vagrant/cloudwatch-agent-setup.sh
@@ -30,7 +30,7 @@ apt-get -y update && apt-get -y install collectd
 #apt-get -y update && apt-get -y install jq
 echo "current_directory_start"
 pwd
-cd /tmp
+#cd /tmp
 
 # Get the latest stable jq release URL from GitHub releases page (check for the latest version)
 # As of now, jq 1.7.1 is the latest stable.
