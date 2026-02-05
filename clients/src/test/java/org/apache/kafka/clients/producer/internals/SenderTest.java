0a0fa9dcb96aa4d78e782d02ad43b68379b049cd
Manikumar Reddy <manikumar.reddy@gmail.com>
2026-02-05 22:28:55 +0530

    MINOR: Remove unused imports

--- clients/src/test/java/org/apache/kafka/clients/producer/internals/SenderTest.java
+++ clients/src/test/java/org/apache/kafka/clients/producer/internals/SenderTest.java
@@ -129,11 +129,9 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class SenderTest {
     private static final int MAX_REQUEST_SIZE = 1024 * 1024;
